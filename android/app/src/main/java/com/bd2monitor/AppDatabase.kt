f60286f88279be1dea68e8fcf02f4f1e2848a198
amin14c <amin14c@gmail.com>
2026-03-21 21:39:11 +0100

    Create AppDatabase.kt

---
+++ android/app/src/main/java/com/bd2monitor/AppDatabase.kt
@@ -0,0 +1,33 @@
+package com.bd2monitor
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [DailyRecord::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun recordDao(): RecordDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+           val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "bd2_monitor_db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
