42240d264e6c4cecbdad63c728407fd223e15278
amin14c <amin14c@gmail.com>
2026-03-23 00:53:55 +0100

    Create PatientProfile.kt

---
+++ android/app/src/main/java/com/bd2monitor/PatientProfile.kt
@@ -0,0 +1,17 @@
+package com.bd2monitor
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "patient_profile")
+data class PatientProfile(
+    @PrimaryKey
+    val id: Int = 1,
+    val fullNameAr: String = "",
+    val fullNameFr: String = "",
+    val birthDate: String = "",
+    val diagnosis: String = "",
+    val doctorName: String = "",
+    val emergencyContact: String = "",
+    val notes: String = ""
+)
